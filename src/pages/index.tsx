c785aad61d6e2231a4f419693eb65fc95cc02bac
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-12-05 22:42:50 -0300

    fix: layout fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Começe usando nossa API acessando&nbsp;
+        <p className="fixed left-0 right-0 m-auto top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Teste nossa API acessando&nbsp;
           <Link href="/api/items/subs">
             <code className="font-mono font-bold text-green-200">api/items/subs</code>
           </Link>
